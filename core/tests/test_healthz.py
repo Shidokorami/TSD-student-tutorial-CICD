ed369d8c941bc06dd12bb4c88cf7afa52b356b3b
Mikołaj Kwieciński <ZawiszaSolec@gmail.com>
2026-06-02 15:25:11 +0000

    testing breaking

--- core/tests/test_healthz.py
+++ core/tests/test_healthz.py
@@ -6,4 +6,4 @@ def test_healthz(client):
     # test xd
     resp = client.get("/healthz/")
     assert resp.status_code == 200
-    assert resp.json() == {"status": "ok"}
+    assert resp.json() == {"status": "false"}
